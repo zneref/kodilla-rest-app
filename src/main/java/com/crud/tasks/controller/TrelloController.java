b27dd999b8fde8e8561fa74099f27f55510690df
zneref <zneref@gmail.com>
2018-04-08 19:48:55 +0200

    18.2 solution

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,31 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/v1/trello")
+public class TrelloController {
+
+    @Autowired
+    private TrelloClient trelloClient;
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
+    public List<TrelloBoardDto> getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+        trelloBoards.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+
+        return trelloBoards.stream()
+                .filter(trelloBoardDto -> trelloBoardDto.getName().contains("Kodilla"))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -0,0 +1,47 @@
+package com.crud.tasks.trello.client;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class TrelloClient {
+    @Value("${trello.api.endpoint.prod}")
+    private String trelloApiEndpoint;
+
+    @Value("${trello.app.key}")
+    private String trelloAppKey;
+
+    @Value("${trello.app.token}")
+    private String trelloToken;
+
+    @Value("${trello.app.user}")
+    private String trelloUser;
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public List<TrelloBoardDto> getTrelloBoards() {
+        URI url = getTrelloBoardsUrl();
+        Optional<TrelloBoardDto[]> boardsResponse = Optional.ofNullable(restTemplate.getForObject(url, TrelloBoardDto[].class));
+        return boardsResponse.map(Arrays::asList).orElseGet(ArrayList::new);
+    }
+
+    private URI getTrelloBoardsUrl() {
+        return UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUser + "/boards")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", trelloToken)
+                .queryParam("fields", "name,id")
+                .queryParam("lists", "all").build().encode().toUri();
+    }
+}
